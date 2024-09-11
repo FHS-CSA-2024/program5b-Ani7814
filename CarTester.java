4eac6cdace80083052a2a4a3179260a32a6d8865
Ani7814 <anir7814@gmail.com>
2024-09-10 22:13:01 -0700

    Update CarTester.java

--- CarTester.java
+++ CarTester.java
@@ -1,1 +1,48 @@
+import java.util.Scanner;
+
+public class carTester{
+
+    
+    
+    
+    
+    public static void main (String[] args){
+        String name;
+        double miles;
+        double gallons;
+        
+        Scanner info = new Scanner(System.in);
+        
+        System.out.println("Please enter the car's name: ");
+        name = info.nextLine();
+        System.out.println("Please enter cars miles: ");
+        miles = info.nextDouble();
+        System.out.println("Please enter cars gallons: ");
+        gallons = info.nextDouble();
+        
+        Car car1 = new Car(name, miles, gallons);
+        System.out.println(car1.toString());
+        
+        System.out.println("----------------Next Car-----------------------");
+        
+        String name2;
+        double miles2;
+        double gallons2;
+        
+        System.out.println("Please enter the car's name: ");
+        name2 = info.nextLine();
+        System.out.println("Please enter cars miles: ");
+        miles2 = info.nextDouble();
+        System.out.println("Please enter cars Gallons: ");
+        gallons2 = info.nextDouble();
+        
+        Car car2 = new Car(name2, miles2, gallons2);
+        System.out.println(car2.toString());
+    }
+    
+    
+    
+    
+    
+}
 
