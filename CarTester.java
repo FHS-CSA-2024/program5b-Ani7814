32bc47e8485bd73247865a83c0ea61b87e44e487
Ani7814 <anir7814@gmail.com>
2024-09-12 16:37:28 -0700

    Update CarTester.java

--- CarTester.java
+++ CarTester.java
@@ -2,40 +2,44 @@ import java.util.Scanner;
 
 public class carTester{
     Scanner info = new Scanner(System.in);
-    public void addCar(){
+    public Car addCar(){
         System.out.println("Please enter the car's name: ");
         String name = info.nextLine();
         System.out.println("Please enter cars miles: ");
         Double miles = info.nextDouble();
         System.out.println("Please enter cars gallons: ");
         Double gallons = info.nextDouble();
-        
-        Car car1 = new Car(name, miles, gallons);
-        car1.toString();
+        return new Car(name, miles, gallons);
     }
-    
-    /*public void addCar(){
-        System.out.println("Please enter the car's name: ");
-        String name = info.nextLine();
-        System.out.println("Please enter cars miles: ");
-        Double miles = info.nextDouble();
-        System.out.println("Please enter cars gallons: ");
-        Double gallons = info.nextDouble();
-        
-        Car car1 = new Car(name, miles, gallons);
-        car1.toString();
-    }*/
     
     public static void main (String[] args){
         
-        Car1.carTester = new carTester();
+        Car car1 = new carTester().addCar();
+        
+        System.out.println(car1);
+        
+        System.out.println("-----------------NEXT CAR--------------");
+        Car car2 = new carTester().addCar();
+        System.out.println(car2);
         
         
-        
-    }
-    
-    
-    
-    
-    
+    }   
 }
+
+/*
+Please enter the car's name: 
+Honda Civic
+Please enter cars miles: 
+234
+Please enter cars gallons: 
+100
+Honda Civic averaged 2.3 m/s
+-----------------NEXT CAR--------------
+Please enter the car's name: 
+Toad Mobile
+Please enter cars miles: 
+13
+Please enter cars gallons: 
+7
+Toad Mobile averaged 1.9 m/s
+*/
