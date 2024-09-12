771afe6742b59e2d3ac62e1792502d4938fbd005
Ani7814 <anir7814@gmail.com>
2024-09-12 12:31:26 -0700

    Update CarTester.java

--- CarTester.java
+++ CarTester.java
@@ -1,43 +1,37 @@
 import java.util.Scanner;
 
 public class carTester{
-
-    
-    
-    
-    
-    public static void main (String[] args){
-        String name;
-        double miles;
-        double gallons;
-        
-        Scanner info = new Scanner(System.in);
-        
+    Scanner info = new Scanner(System.in);
+    public void addCar(){
         System.out.println("Please enter the car's name: ");
-        name = info.nextLine();
+        String name = info.nextLine();
         System.out.println("Please enter cars miles: ");
-        miles = info.nextDouble();
+        Double miles = info.nextDouble();
         System.out.println("Please enter cars gallons: ");
-        gallons = info.nextDouble();
+        Double gallons = info.nextDouble();
         
         Car car1 = new Car(name, miles, gallons);
-        System.out.println(car1.toString());
-        
-        System.out.println("----------------Next Car-----------------------");
-        
-        String name2;
-        double miles2;
-        double gallons2;
-        
+        car1.toString();
+    }
+    
+    /*public void addCar(){
         System.out.println("Please enter the car's name: ");
-        name2 = info.nextLine();
+        String name = info.nextLine();
         System.out.println("Please enter cars miles: ");
-        miles2 = info.nextDouble();
-        System.out.println("Please enter cars Gallons: ");
-        gallons2 = info.nextDouble();
+        Double miles = info.nextDouble();
+        System.out.println("Please enter cars gallons: ");
+        Double gallons = info.nextDouble();
         
-        Car car2 = new Car(name2, miles2, gallons2);
-        System.out.println(car2.toString());
+        Car car1 = new Car(name, miles, gallons);
+        car1.toString();
+    }*/
+    
+    public static void main (String[] args){
+        
+        Car1.carTester = new carTester();
+        
+        
+        
     }
     
     
@@ -45,4 +39,3 @@ public class carTester{
     
     
 }
-
